8220eebefbf68a690da0736438feea67bcd4c6fe
Lilly Perez <lillyperez9@icloud.com>
2025-10-18 22:05:46 -0400

    Made API project withe node.js & NPM

--- script.js
+++ script.js
@@ -19,4 +19,54 @@ async function fetchArtwork(){
 }
 
 fetchArtwork();
+
+
+//Artwork 
+
+// Reusing ARTWORK_ID and API_URL declared earlier
+
+const artworkInfoButton = document.querySelector("#artwork-info-button");
+const artworkImageButton = document.querySelector("#artwork-image-button");
+
+const imgEl = document.querySelector(".artwork-img");
+const imgTitle = document.querySelector(".img-title");
+
+const imgDescription = document.querySelector(".img-description");
+const copyrightText = document.querySelector(".copyright");
+const IMAGE_SIZE = 600;
+
+async function fetchArtistImage(){
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const data = await response.json();
+    const imageId = data.data.image_id;
+    const baseURL =data.config.iiif_url;
+    const fullImageURL = `${baseURL}/${imageId}/full/${IMAGE_SIZE},/0/default.jpg`;
+    imgEl.src = fullImageURL;
+  }
+  catch (error) {
+    console.error("Fetch error:", error);
+}
+}
+//Artwork Info//
+async function fetchArtworkInfo(){
+  try{
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error("Network response was not ok");
+    const {data} = await response.json();
+    const {title, description} = data;
+
+    imgTitle.textContent = title;
+    imgDescription.innerHTML=description;
     
+  }
+  catch (error) {
+    console.error("Fetch error:", error);
+}
+}
+
+//Button Event Listeners//
+artworkImageButton.addEventListener('click', () => { fetchArtistImage(); });
+
+artworkInfoButton.addEventListener('click', () =>  {fetchArtworkInfo(); });
